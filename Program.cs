eec470cf1fa17530653f4f383515d2b59b922909
NiButera <nikolasxp683@gmail.com>
2023-03-19 03:15:42 -0300

    Impressora Curiosa

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("O que , o que é?");
+
+Thread.Sleep(2000); 
+Console.WriteLine("Uma impressora disse para a outra.");
+
+Thread.Sleep(5000);
+Console.WriteLine("Essa folha é sua, ou é impressão minha?");
